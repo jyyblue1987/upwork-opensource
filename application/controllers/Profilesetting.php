132625ad793febd9a8f9e8e9474d545aee5bd54f
winjobllc <winjobllc@gmail.com>
2017-02-28 23:08:03 +0600

    profile setting

--- application/controllers/Profilesetting.php
+++ application/controllers/Profilesetting.php
@@ -16,23 +16,35 @@ class Profilesetting extends CI_Controller {
             if($this->Adminlogincheck->checkx()){
                 //webuser details//
                 $condition = " AND webuser_id=".$this->session->userdata(USER_ID);
+                // add by indsys tech start
+                $sql = "SELECT cropped_image FROM webuser WHERE webuser_id =  " . $this->session->userdata(USER_ID);
+                $croppedImage =    $this->db->query($sql)->row();
+                // add by indsys tech end
                 $webUserContactDetails = $this->common_mod->get(WEB_USER_ADDRESS,null,$condition);
+                 
+                if(!empty($webUserContactDetails)){
+				
+			    $country_id = $webUserContactDetails['rows'][0]['country'];
+				$countrydailing = $this->common_mod->get_record_by_id(COUNTEY_TABLE,$country_id);	
+				}
+           
                 if(empty($webUserContactDetails['rows'])){
                     $webUserContactDetails['rows'][0] = "";
                 }
 
                 //webuser contact details//
                 $webuserCountry = $this->common_mod->getSpecificColVal(COUNTEY_TABLE,"country_name"," AND country_id  =".$this->session->userdata('webuser_country'));
+              
                 $countryList = $this->common_mod->get(COUNTEY_TABLE,null," AND country_status=1");
                 $timezones = $this->timezone->get_timezones();
                 $timezone = $this->timezone->get((int)$webUserContactDetails['rows'][0]['timezone']);
-
                 $data = array(
                     'title' => "Profile Setting",
                     'page' => "profilesetting",
                     'name' => $this->session->userdata('fname')." ".$this->session->userdata('lname'),
                     'username' => $this->session->userdata('username'),
                     'id' => $this->session->userdata('id'),
+                    'croppedImage' =>$croppedImage, // ad by indsys tech
                     'js' =>array(),
                     'jsf' =>array("assets/js/layerslider.transitions.js","assets/js/layerslider.kreaturamedia.jquery.js","assets/js/owl.carousel.min.js","assets/js/homepage.js"),
                     'css' =>array("assets/css/layerslider.css","assets/css/owl.carousel.css","assets/css/owl.theme.css"),
@@ -43,12 +55,40 @@ class Profilesetting extends CI_Controller {
                     'webuserCountry'=>$webuserCountry,
                     'timezone' => $timezone,
                     'timezones' => $timezones,
-					'profile'=>$profile
+					'profile'=>$profile,
+					'country_code_dailing' =>$countrydailing->country_dialingcode
                 );
+            
+                
                     $this->Admintheme->webview("profilesetting",$data);
                 }else{
                     redirect(site_url("signin"));
                 }
 	}
+	
+	/***************Indsys Technologies 23/02/2017  country_dialingcode***************/
+	public function getcountry_dialingcode(){
+		$result = array();
+		if($_POST['country_dialingcode']){
+			$this->db->select('country_id,country_dialingcode');
+			$this->db->from(COUNTEY_TABLE);
+			$this->db->where('country_id',$_POST['country_dialingcode']);
+			$country_dialingcode = $this->db->get()->row();
+             if($country_dialingcode){
+				$Country_code = $country_dialingcode->country_dialingcode;
+				$key ='sucsses';
+			 }
+			}else{
+			$key ='faild';	
+				
+			}
+			$result = array('key' =>$key,'country_dialingcode'=>$Country_code);
+			echo json_encode($result);
+			
+
+		
+		}
+		
+		/****************************************************************/
 }
 
