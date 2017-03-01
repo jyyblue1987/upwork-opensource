4aaaeba399cf9d3929c8f0c6428338cdb4b516b2
winjobllc <winjobllc@gmail.com>
2017-03-01 17:35:54 +0600

    helper

--- application/helpers/datetime_helper.php
+++ application/helpers/datetime_helper.php
@@ -23,4 +23,39 @@ class DatetimeHelper {
 
         return $return;
     }
+
+    public static function timeElapsedString($ptime) {
+        $etime = time() - $ptime;
+
+        if ($etime < 1) {
+            return '0 seconds';
+        }
+
+        $a = array(
+            365 * 24 * 60 * 60 => 'year',
+            30 * 24 * 60 * 60 => 'month',
+            24 * 60 * 60 => 'day',
+            60 * 60 => 'hour',
+            60 => 'minute',
+            1 => 'second'
+        );
+
+        $a_plural = array(
+            'year' => 'years',
+            'month' => 'months',
+            'day' => 'days',
+            'hour' => 'hours',
+            'minute' => 'minutes',
+            'second' => 'seconds'
+        );
+
+        foreach ($a as $secs => $str) {
+            $d = $etime / $secs;
+
+            if ($d >= 1) {
+                $r = round($d);
+                return $r . ' ' . ($r > 1 ? $a_plural[$str] : $str) . ' ago';
+            }
+        }
+    }
 }
