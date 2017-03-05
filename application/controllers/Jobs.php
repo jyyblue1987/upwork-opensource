fc073d3bf2ea19379f60d0c2627949cf8fe2ccef
Hermannovich <donfack.hermann@gmail.com>
2017-03-05 19:11:36 +0000

    Jobs.php edited online with Bitbucket

--- application/controllers/Jobs.php
+++ application/controllers/Jobs.php
@@ -1253,7 +1253,7 @@ class Jobs extends CI_Controller {
             // added by (Donfack Zeufack Hermann) end
             
             // added by (Donfack Zeufack Hermann) start reduce the number line of code to pass data to the view.
-            $this->Admintheme->webview2("jobs/fixed_client_view", compact('job_status', 'ststus', 'payments'));
+            $this->Admintheme->webview_2("jobs/fixed_client_view", compact('job_status', 'ststus', 'payments'));
             // added by (Donfack Zeufack Hermann) end
         }
     }
