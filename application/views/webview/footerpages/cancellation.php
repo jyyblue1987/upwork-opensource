b051da90d24ad9903b9f8ace08db92b4030ed6b7
avillanueva <avillanueva@LWCB-CZC1126C24.united.domain>
2017-03-14 23:09:47 +0800

    - added sample pages for the footer.

---
+++ application/views/webview/footerpages/cancellation.php
@@ -0,0 +1,5 @@
+<div style="word-wrap: wrap; padding: 20px; text-align: justify">
+
+    <h1 style="padding-bottom: 20px;"><?= $title; ?></h1>
+    In some instances, refund of fees may be done as bonus credit. This bonus may only be used on site, and can not be withdrawn or transferred. The bonus will expire in 90 days from the date of receipt.
+</div>
