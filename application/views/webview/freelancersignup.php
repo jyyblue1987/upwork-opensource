76a5f06397a6e3d6631c6b68b3b24dac7cc57eb7
rapidoodle <ralfhbryanperez@yahoo.com>
2017-03-20 03:17:14 +0800

    responsive login signup

--- application/views/webview/freelancersignup.php
+++ application/views/webview/freelancersignup.php
@@ -2,89 +2,91 @@
 .help-block{font-size: 16px;font-family: calibri;}
 </style>
 
+
+
 <div class="custom_employeersignup main_area_div">
-   <div class="row"> 
-	    <div class="col-md-12">
-		   <div class="main_area_div_sub_div1">
-		     <h1>Create a Free Freelancer Account </h1>
-			 		<p>Looking to hire? <a href="<?php echo site_url("employeersignup/") ?>">Sign up as a Employer</a> </p>
-			<?php if(isset($_GET['email'])){ ?>
-		<p>Email Allready Exists </p>
-		<?php } ?>
-			<?php if(isset($_GET['username'])){ ?>
-		<p>Username Allready Exists </p>
-		<?php } ?>
-		
-	   <form id="basic" method="post" action="<?php echo site_url("registercheck"); ?>">
-<input type="hidden" name="type" value="2">
+    <div class="row">
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 hidden-xs">
+    </div>
+    <div class="col-lg-6 col-md-6 col-sm-6 col-xs-12 no-pad" >
+        <div class="main_area_div_sub_div1">
+        <center><h1>Create a Free Freelancer Account </h1>
+        <p>Looking for hire? <a href="<?php echo site_url("freelancersignup/") ?>">Sign up as a Employer</a> </p>
+        <?php if(isset($_GET['email'])){ ?>
+        <p>Email Allready Exists </p>
+        <?php } ?>
+        <?php if(isset($_GET['username'])){ ?>
+        <p>Username Allready Exists </p>
+        <?php } ?>
+        </center>
+            <form id="basic" method="post" action="<?php echo site_url("registercheck"); ?>">
+            <input type="hidden" name="type" value="1">
+            <div class="form-group">
+                <input type="text" name="fname" value="" class="form-control" id="firstname" autocomplete="false" placeholder="First Name">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="lname" value="" id="lastname" placeholder="Last Name">
+            </div>
+            <div class="form-group">
+                <input type="text" name="fname" value="" class="form-control" id="firstname" autocomplete="false" placeholder="First Name">
+            </div>
+            <div class="form-group">
+                <input type="text" class="form-control" name="lname" value="" id="lastname" placeholder="Last Name">
+            </div>
+            <?php           
 
-             
-                        <div class="col-md-10 col-xs-12 form-group">
-                                                            <input type="text" name="fname" value="" class="form-control" id="firstname" autocomplete="false" placeholder="First Name">
-                                                        </div>
-                        <div class="col-md-10 col-xs-12 form-group">
-                                                            <input type="text" class="form-control" name="lname" value="" id="lastname" placeholder="Last Name">
-                                                        </div>
-											<?php			
-														
-			  echo $this->Adminforms->selectdbnewxgdcv("Country","country","Select Country","country","id","name","index","asc","",true,array("all","All Country"));
-			  
-?>
-														
-                        <div class="col-md-10 col-xs-12 form-group">
-                                                            <input type="text" class="form-control" name="username" value="" id="username"  placeholder="Username">
-                                                        </div>
-                    <div class="col-md-10 col-xs-12  form-group">
-                        <div class="form-group">
-                                                            <input type="email" class="form-control" id="emaila" value="" placeholder="Email" name="email">
-                                                        </div>
-                    </div>
+            $this->Adminforms->selectdbnewxgdcv("Country","country","Select Country","country","id","name","index","asc","",true,array("all","All Country"));
 
-                    <div class="col-md-10 col-xs-12 form-group ">
-                        <input type="password" value="" name="password" class="form-control" id="password"  placeholder="Password">
-						<div id="pswd_info">
-    <h4>Password must meet the following requirements:</h4>
-    <ul>
-        <li id="letter" class="invalid">At least <strong>one letter</strong></li>
-        <li id="capital" class="invalid">At least <strong>one capital letter</strong></li>
-        <li id="number" class="invalid">At least <strong>one number</strong></li>
-        <li id="length" class="invalid">Be at least <strong>8 characters</strong></li>
-    </ul>
+            ?>
+
+            <div class="form-group">
+                <input type="text" class="form-control" name="username" value="" id="username"  placeholder="Username">
+            </div>
+
+            <div class="form-group">
+                <input type="email" class="form-control" id="emaila" value="" placeholder="Email" name="email">
+            </div>
+
+            <div class="form-group">
+                <input type="password" value="" name="password" class="form-control" id="password"  placeholder="Password">
+            <div id="pswd_info">
+                <h4>Password must meet the following requirements:</h4>
+            <ul>
+                <li id="letter" class="invalid">At least <strong>one letter</strong></li>
+                <li id="capital" class="invalid">At least <strong>one capital letter</strong></li>
+                <li id="number" class="invalid">At least <strong>one number</strong></li>
+                <li id="length" class="invalid">Be at least <strong>8 characters</strong></li>
+            </ul>
+            </div>
+            </div>
+
+
+            <div class="form-group ">
+                <input type="password" value="" name="confirm_password" class="form-control" id="confirm_password"  placeholder="Confirm Password">
+                <div id="pswd_infob">
+                    <ul>
+                        <li id="vefyx" class="invalid">Verify Password</li>
+                    </ul>
+                </div>
+            </div>
+            <div class="form-group ">
+                <div id="captchaContainer" data-sitekey="6Lf-tggUAAAAAOzXu2Ub57Ws-IxAVy_WxEkB6WZ5"></div>
+            </div>
+
+
+            <br><br>
+                <input style="padding: 11px 100px;margin-top: 15px;margin-left:10px!important;" type="submit" value="Get Started" id="next" class="btn btn-success">
+            <!--<button type="submit" class="btn btn-primary pull-right">Next</button>--> 
+                <h2>By creating an account, you agree to our</h2>
+                <h3><a href="#">Winjob Marketplace User Agreement</a> <span>and</span> <a href="#">Privacy policy</a></h3>
+            </form>
+        </div>
+    </div>
+    <div class="col-lg-3 col-md-3 col-sm-3 col-xs-12 hidden-xs">
+    </div>
+    </div>
 </div>
-                    </div>
 
-
-                    <div class="col-md-10 col-xs-12 form-group ">
-                        <input type="password" value="" name="confirm_password" class="form-control" id="confirm_password"  placeholder="Confirm Password">
-						
-
-<div id="pswd_infob">
-    <ul>
-        <li id="vefyx" class="invalid">Verify Password</li>
-    </ul>
-</div>
-                    </div>
-                    <div class="col-md-10 col-xs-12 form-group ">
-						<div style="margin-left: 46px;" id="captchaContainer" data-sitekey="6Lf-tggUAAAAAOzXu2Ub57Ws-IxAVy_WxEkB6WZ5"></div>
-
-						
-
-                    </div>	
-          
-	<br><br>
-                    <div class="col-md-12"><input style="margin-left: 46px;padding: 11px 100px;margin-top: 15px;" type="submit" value="Get Started" id="next" class="btn btn-success"></div>
-                    <!--<button type="submit" class="btn btn-primary pull-right">Next</button>--> <div class="col-xs-12  col-sm-12  col-md-12">
-			   <h2>By creating an account, you agree to our</h2>
-			   <h3><a href="#">Winjob Marketplace User Agreement</a> <span>and</span> <a href="#">Privacy policy</a></h3>
-			 </div>
-                </form>
-			
-		   </div> 
-		   
-		   
-		 </div>
-	 </div><!-- row-->
-   </div>
    
 <style type="text/css">
 #mid_contant {
--- application/views/webview/signup.php
+++ application/views/webview/signup.php
@@ -2,62 +2,40 @@
 		<h1 class="text-center">Let's Get Started!</h1>
 		<h3 class="text-center">First Choose an Account What you'r Looking for.</h3>
 		
-		<div class="row">
-		
-		<div class="col-md-12">
-		<div class="row">
-		<div class="col-md-5  col-sm-12 text-center" style="    padding: 50px 0px;">
-		
-		<div style="font-size:100px;color:#1ca8c6;">
-		<i class="fa fa-user-secret" aria-hidden="true"></i>
+		<div class="row marg-bot-40">
+
+		<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 text-center">
+			<div class="signup-icon">
+				<i class="fa fa-user-secret" aria-hidden="true"></i>
+			</div>
+			<div style="font-family: calibri;font-size: 18px;" class="o-user-type-selection">
+				I want to hire a Professional
+			</div>
+			<p style="font-family: calibri;font-size: 18px;margin-bottom: 40px;" class="fs-sm m-lg-bottom">
+				Find, collaborate with, <br> and pay an expert.
+			</p>
+
+			<a class="custom_signup_button btn btn-success"  href="<?php echo site_url("employeersignup/") ?>">Sign up as Employer</a>
 		</div>
-		
-		<div style="font-family: calibri;font-size: 18px;" class="o-user-type-selection">I want to hire a Professional</div>
+
+		<div class="col-lg-2 col-md-2 col-sm-12 col-xs-12 or-container">
+			<p class="or">OR</p>
+		</div>
+
+		<div class="col-lg-5 col-md-5 col-sm-12 col-xs-12 text-center	">
+		<div class="signup-icon">
+			<i class="fa fa-desktop " aria-hidden="true"></i>
+		</div>
+
+
+		<div style="font-family: calibri;font-size: 18px;" class="o-user-type-selection">
+			I'm looking for online work</div>
 		<p style="font-family: calibri;font-size: 18px;margin-bottom: 40px;" class="fs-sm m-lg-bottom">
-                        Find, collaborate with,
-                        <br>
-                        and pay an expert.
-                    </p>
-		
-		<a class="custom_signup_button btn btn-success"  href="<?php echo site_url("employeersignup/") ?>">Sign up as Employer</a>
-		</div>
-
-		
-	    <div class="col-sm-12 col-md-2">
-		<div class="main_area_div_sub_div2_main">
-		    <div class="main_area_div_sub_divx2">
-		      <div></div>  
-			  <div><p>OR</p></div>
-			  <div></div>
-		   </div>
-		   
-		</div>
-		</div>
-		 
-		 
-		<div class="col-md-5 col-sm-12 text-center" style="    padding: 50px 0px">
-		
-		
-		<div style="font-size:100px;color:#1ca8c6;">
-		<i class="fa fa-desktop " aria-hidden="true"></i>
-		</div>
-		
-		
-		<div style="font-family: calibri;font-size: 18px;" class="o-user-type-selection">I'm looking for online work</div>
-		<p style="font-family: calibri;font-size: 18px;margin-bottom: 40px;" class="fs-sm m-lg-bottom">
-                       Find freelance projects and <br>
-grow your business.
-
-
-                    </p>
-		
+			Find freelance projects and <br> grow your business.
+		</p>
 		<a class="custom_signup_button btn btn-success"  href="<?php echo site_url("freelancersignup/") ?>">Sign up as Freelancer</a>
-		
-		
 		</div>
 		</div> 	
-	 </div> 	
-	 </div> 	
 	 </div> 	
 	 
 <style type="text/css">
