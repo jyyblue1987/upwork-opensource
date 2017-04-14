f446c8e82d5bc0412ab7a77aa45440a1295d336a
avillanueva <avillanueva@LWCB-CZC1126C24.united.domain>
2017-04-14 12:12:21 +0800

    - added back header4.php, footer2.php in layout folder.

---
+++ application/views/webview/layout/footer2.php
@@ -0,0 +1,75 @@
+ 
+  
+  </div>
+</section>
+<!-- End main_Div-->
+<div class="clear"> </div>
+</section>
+<!-- End main_Div-->
+<div class="clear"> </div>
+<section class="big_footer">
+<div id="find-jobs_container" class="container">
+    <div class="row">
+	 <div class="col-sm-6 col-md-6 col-lg-3">
+	    <div class="footer1">  
+			 <ul>
+			 <li><p><b>Winjob</b></p></li>
+			 <li><a href="<?php echo site_url("aboutus"); ?>">About us</a></li>
+			 <li><a href="<?php echo site_url("contact"); ?>">Contact us</a></li>
+			 <li>Press</li>
+			 <li>Enterprise Soluation</li>
+			 <li>Feedback</li>
+			</ul>
+		</div>
+	 </div>
+  <div class="col-sm-6 col-md-6 col-lg-3">
+	       <div class="footer1">  
+			 <ul>
+			  <li><p><b>Contact Support</b></p></li>
+			 <li>Help & Support</li>
+			 <li>Trust and Safety</li>
+			 <li>Help Make Winjob better</li>
+			</ul>
+		 </div>
+	 </div>
+	 <div class="clear1"></div>
+	<div class="col-sm-6 col-md-6 col-lg-3">
+	      <div class="footer1">  
+			 <ul>
+			 <li><p><b>Information</b></p></li>
+			  <li>Fee and Charges</li>
+			  <li>Cancellations & Refunds</li>
+			  <li><a href="<?php echo site_url("terms"); ?>">Terms & Conditions</a></li>
+			  <li><a href="<?php echo site_url("policy"); ?>">Privacy Policy</a></li>
+			  <li>Desktop App</li>
+			</ul>
+		 </div>
+	 </div>
+
+	<div class="col-sm-6 col-md-6 col-lg-3">
+	      <div class="footer1">  
+			
+			<ul>
+			<li><p><b>Knowledgebase</b></p></li>
+				<li>How to Join us?</li>
+				<li>How to create support tickets?</li>
+				<li>How to get your work done?</li>
+				<li>How to add fund?</li>
+			</ul>
+		 </div>
+	 </div>
+	</div>
+</div>
+
+
+<!-- End big_footer---->
+<div class="footer">
+  <div id="find-jobs_container" class="container">
+     <div class="row">
+	    <div class="col-sm-6 col-md-6"><h1><b>WINJOB</b></h1></div>
+	    <div class="col-sm-6 col-md-6"><p>© 2016 WINJOB</p></div>
+	 </div>
+  </div>
+</div>
+<!-- End footer---->
+</section>
